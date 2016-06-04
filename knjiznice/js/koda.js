e52449bab8d3df7138e8091d778765f2d10562e7
Rok Pajnič <Rok Pajni>
2016-06-04 19:37:25 +0200

    Dodan AwesomeFont,  BAI; narejen UserCreate in GetUser

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -38,10 +38,8 @@ function generirajPodatke(stPacienta) {
   return ehrId;
 }
 
-
 // TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
 function addCilj(){
-  sessionId = getSessionId();
   
   var li = document.createElement("li");
   var input = document.getElementById("ciljiInput");
@@ -54,13 +52,212 @@ function addCilj(){
 }
 
 function addUser() {
-  var ime = $("#newIme").val();
-	var priimek = $("#newPriimek").val();
-	var datumRojstva = $("#newDatumRojstva").val();
-  var teza = $("#newTeza").val();
-  var visina = $("#newVisina").val();
-  var misicnaMasa = $("#newMisicnaMasa").val();
-  var kometar = $("newComment")
+    sessionId = getSessionId();
+    
+    var ime = $("#newIme").val();
+    var priimek = $("#newPriimek").val();
+    var datumRojstva = $("#newDatumRojstva").val();
+    var teza = $("#newTeza").val();
+    var visina = $("#newVisina").val();
+    var mascoba = $("#newMascoba").val();
+    var kometar = $("newComment").val();
+    
+    if (!ime || !priimek || !datumRojstva || !teza || !visina || ime.trim().length == 0 ||
+      priimek.trim().length == 0 || datumRojstva.trim().length == 0) {
+		$("#kreirajSporocilo").html("<span class='obvestilo label " +
+      "label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+	} else {
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		$.ajax({
+		    url: baseUrl + "/ehr",
+		    type: 'POST',
+		    success: function (data) {
+		        var ehrId = data.ehrId;
+                newEHRid = ehrId;
+		        var partyData = {
+		            firstNames: ime,
+		            lastNames: priimek,
+		            dateOfBirth: datumRojstva,
+		            partyAdditionalInfo: [{
+                        key: "ehrId",
+                        value: ehrId}]
+		        };
+		        $.ajax({
+		            url: baseUrl + "/demographics/party",
+		            type: 'POST',
+		            contentType: 'application/json',
+		            data: JSON.stringify(partyData),
+		            success: function (party) {
+		                if (party.action == 'CREATE') {
+		                //     $("#kreirajSporocilo").html("<span class='obvestilo " +
+                        //   "label label-success fade-in'>Uspešno kreiran EHR '" +
+                        //   ehrId + "'.</span>");
+		                   document.getElementById('newEHR').innerHTML = ehrId;
+                            console.log(ehrId);
+                            document.getElementById('upload').disabled = false;
+                            document.getElementById('create').disabled = true;
+		                }
+		            },
+		            error: function(err) {
+		            	$("#newEHR").html("<span class='obvestilo label " +
+                    "label-danger fade-in'>Napaka '" +
+                    JSON.parse(err.responseText).userMessage + "'!");
+		            }
+		        });
+		    }
+		});
+	}
+    
+    document.getElementById('upload').disabled = false;
+    document.getElementById('create').disabled = true;
+}
+
+function uploadData(newUser) {
+	sessionId = getSessionId();
+    
+    if(newUser == 1) {
+        console.log("new user");
+        var ehrId = document.getElementById('newEHR').innerHTML;
+        var datumInUra = getDate();
+        var telesnaTeza = $("#newTeza").val();
+        var obsegPasu = $("#newObsegPasu").val();
+        var mascoba = $("#newMascoba").val();
+        var telesnaVisina = $("#newVisina").val();
+        var komentar = $("#newComment").val();
+        console.log(ehrId + ' ' + datumInUra + ' ' + telesnaVisina + 
+        ' ' +telesnaTeza + ' '+ obsegPasu + ' ' + mascoba + ' ' + komentar);
+    }
+    else {
+        console.log("existing user");
+        var ehrId = $("#EHRid").val();;
+        var datumInUra =  $("#datum").val();
+        var telesnaTeza = $("#teza").val();
+        var mascoba = $("#mascoba").val();
+        var telesnaVisina = $("#visina").val();
+        var obsegPasu = $("#obsegPasu").val();
+        var komentar = $("#comment").val();
+    }
+    console.log(ehrId);
+    console.log(ehrId.length);
+	if (!ehrId || ehrId.trim().length == 0) {
+		$("#saveFeedback").html("<span class='obvestilo " +
+      "label label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+	} else {
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		var podatki = {
+			// Struktura predloge je na voljo na naslednjem spletnem naslovu:
+      // https://rest.ehrscape.com/rest/v1/template/Vital%20Sign    s/example
+		    "ctx/language": "en",
+		    "ctx/territory": "SI",
+		    "ctx/time": datumInUra,
+		    "vital_signs/height_length/any_event/body_height_length": telesnaVisina,
+		    "vital_signs/body_weight/any_event/body_weight": telesnaTeza,
+            //systolic je v mojem primeru obsegPasu
+		    "vital_signs/blood_pressure/any_event/systolic": obsegPasu,
+            // diastolic je v mojem primeru mascoba
+		    "vital_signs/blood_pressure/any_event/diastolic": mascoba,
+
+		};
+		var parametriZahteve = {
+		    ehrId: ehrId,
+		    templateId: 'Vital Signs',
+		    format: 'FLAT',
+		    committer: komentar
+		};
+		$.ajax({
+		    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+		    type: 'POST',
+		    contentType: 'application/json',
+		    data: JSON.stringify(podatki),
+		    success: function (res) {
+		        $("#saveFeedback").html(
+              "<span class='obvestilo label label-success fade-in'>" +
+              res.meta.href + ".</span>");
+              document.getElementById('newFeedback').innerHTML = 'Podatki uspešno naloženi';
+		    },
+		    error: function(err) {
+		    	$("#saveFeedback").html(
+            "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+            JSON.parse(err.responseText).userMessage + "'!");
+		    }
+		});
+	}
+}
+
+function getUser() {
+    sessionId = getSessionId();
+
+	var ehrId = $("#searchEHR").val();
+    var uporabnik;
+    if (!ehrId || ehrId.trim().length == 0) {
+        $("#saveFeedback").html('<div class="alert alert-warning" style=" margin-bottom:0px; padding-bottom:5px; padding-top:5px" role="alert">Prosim vnesite zahtevan podatek!</div>')
+	} else {
+		$.ajax({
+			url: baseUrl + "/demographics/ehr/" + ehrId + "/party",
+			type: 'GET',
+			headers: {"Ehr-Session": sessionId},
+	    	success: function (data) {
+                //.firstNames = ime; .lestNames = priimek; dateOfBirth = rojstvo;
+				uporabnik = data.party;
+                console.log(uporabnik);
+				$("#saveFeedback").html('<div class="alert alert-success" style=" margin-bottom:0px; padding-bottom:5px; padding-top:5px" role="alert">Uporabnik se je uspešno naložil</div>');
+			   document.getElementById('EHRid').value = ehrId;
+               document.getElementById('ime').value = uporabnik.firstNames;
+               document.getElementById('priimek').value = uporabnik.lastNames;
+               document.getElementById('datumRojstva').value = uporabnik.dateOfBirth.substring(0,10);
+            },
+			error: function(err) {
+				$("#saveFeedback").html('<div class="alert alert-danger" style=" margin-bottom:0px; padding-bottom:5px; padding-top:5px" role="alert">Napaka: ' +
+          JSON.parse(err.responseText).userMessage + "'!</div>");
+			}
+		});
+	}
+}
+
+function getUserData() {
+    sessionId = getSessionId();
+
+	var ehrId = $("#EHRid").val();
+    
+}
+
+
+function clearCreate() {
+    document.getElementById('newIme').value = '';
+    document.getElementById('newPriimek').value = '';
+    document.getElementById('newDatumRojstva').value = '';
+    document.getElementById('newTeza').value = '';
+    document.getElementById('newMascoba').value = '';
+    document.getElementById('newVisina').value = '';
+    document.getElementById('newObsegPasu').value = '';
+    document.getElementById('newComment').value = '';
+    
+    $('#newUser').on('hidden', function() {
+        $(this).removeData('modal');
+    });
+}
+
+
+function getDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    
+    if(dd<10) {
+    dd='0'+dd
+    } 
+
+    if(mm<10) {
+        mm='0'+mm
+    } 
+    
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
 }
 
 function BMI() {
@@ -71,6 +268,37 @@ function BMI() {
     bmi = bmi.toFixed(2);
     console.log(bmi);
     document.getElementById("bmi").value = bmi;
+    
+    if(bmi < 18.5) {
+        document.getElementById('BMIfeedback').innerHTML = '<div class="alert alert-danger" style="margin-top:10px; margin-bottom:0px; padding-bottom:10px; padding-top:10px" role="alert">Sodeč po BMI ste podhranjeni</div>';
+    }
+    else if (bmi < 30) {
+        document.getElementById('BMIfeedback').innerHTML = '<div class="alert alert-success" style="margin-top:10px; margin-bottom:0px; padding-bottom:10px; padding-top:10px" role="alert">Sodeč po BMI ste normalne teže</div>';        
+    }
+    else {
+        document.getElementById('BMIfeedback').innerHTML = '<div class="alert alert-danger" style="margin-top:10px; margin-bottom:0px; padding-bottom:10px; padding-top:10px" role="alert">Sodeč po BMI ste predebeli</div>';          
+    }
+  }
+}
+
+function BAI() {
+  var visina = document.getElementById("visina").value/100;
+  var pas = document.getElementById("obsegPasu").value;
+  var bai = pas/(Math.pow(visina, 1.5))-18;
+  if (!isNaN(bai)) {
+    bai = bai.toFixed(2);
+    console.log(bai);
+    document.getElementById("bai").value = bai;
+    
+    if(bai < 8) {
+        document.getElementById('BAIfeedback').innerHTML = '<div class="alert alert-danger" style="margin-top:10px; margin-bottom:0px; padding-bottom:10px; padding-top:10px" role="alert">Sodeč po BAI ste podhranjeni</div>';
+    }
+    else if (bai < 21) {
+        document.getElementById('BAIfeedback').innerHTML = '<div class="alert alert-success" style="margin-top:10px; margin-bottom:0px; padding-bottom:10px; padding-top:10px" role="alert">Sodeč po BAI ste normalne teže</div>';        
+    }
+    else {
+        document.getElementById('BAIfeedback').innerHTML = '<div class="alert alert-danger" style="margin-top:10px; margin-bottom:0px; padding-bottom:10px; padding-top:10px" role="alert">Sodeč po BAI ste predebeli</div>';          
+    }
   }
 }
 
