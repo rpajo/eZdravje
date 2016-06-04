0c8cb44780a8f8d0b5fc742c3273e8d33ebccfeb
Rok Pajnič <Rok Pajni>
2016-06-04 23:06:06 +0200

    Dokončan getUserData in izris 2 grafov

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -64,7 +64,7 @@ function addUser() {
     
     if (!ime || !priimek || !datumRojstva || !teza || !visina || ime.trim().length == 0 ||
       priimek.trim().length == 0 || datumRojstva.trim().length == 0) {
-		$("#kreirajSporocilo").html("<span class='obvestilo label " +
+		$("#saveFeedback").html("<span class='obvestilo label " +
       "label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
 	} else {
 		$.ajaxSetup({
@@ -142,15 +142,15 @@ function uploadData(newUser) {
     console.log(ehrId);
     console.log(ehrId.length);
 	if (!ehrId || ehrId.trim().length == 0) {
-		$("#saveFeedback").html("<span class='obvestilo " +
-      "label label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+		$("#saveFeedback").html("<span class='obvestilo label " +
+      "label-warning fade-in'>Naložite uporabnika z ustreznim erhId</span>");
 	} else {
 		$.ajaxSetup({
 		    headers: {"Ehr-Session": sessionId}
 		});
 		var podatki = {
 			// Struktura predloge je na voljo na naslednjem spletnem naslovu:
-      // https://rest.ehrscape.com/rest/v1/template/Vital%20Sign    s/example
+      // https://rest.ehrscape.com/rest/v1/template/Vital%20Signs/example
 		    "ctx/language": "en",
 		    "ctx/territory": "SI",
 		    "ctx/time": datumInUra,
@@ -160,7 +160,6 @@ function uploadData(newUser) {
 		    "vital_signs/blood_pressure/any_event/systolic": obsegPasu,
             // diastolic je v mojem primeru mascoba
 		    "vital_signs/blood_pressure/any_event/diastolic": mascoba,
-
 		};
 		var parametriZahteve = {
 		    ehrId: ehrId,
@@ -218,13 +217,132 @@ function getUser() {
 	}
 }
 
+
+var dataTeza;
+var dataMascobaPas;
+var dataHeight;
+var normalTeza;
+var normalMascoba;
 function getUserData() {
+    console.log("Fetching data");
     sessionId = getSessionId();
 
 	var ehrId = $("#EHRid").val();
     
+    if (!ehrId || ehrId.trim().length == 0) {
+        $("#saveFeedback").html('<div class="alert alert-warning" style=" margin-bottom:0px; padding-bottom:5px; padding-top:5px" role="alert">Prosim vnesite zahtevan podatek!</div>')
+	} else {
+		$.ajax({
+			url: baseUrl + "/demographics/ehr/" + ehrId + "/party",
+	    	type: 'GET',
+	    	headers: {"Ehr-Session": sessionId},
+	    	success: function (data) {
+				var uporabnik = data.party;
+
+                $.ajax({
+                    url: baseUrl + "/view/" + ehrId + "/" + "weight",
+                    type: 'GET',
+                    headers: {"Ehr-Session": sessionId},
+                    success: function (res) {
+                        if (res.length > 0) {
+                            dataTeza = res;
+                            document.getElementById('teza').value = dataTeza[0].weight;
+                            // console.log(dataTeza);
+                            normalTeza = normalize(dataTeza, "teza");
+                        } else {
+                            $("#saveFeedback").html(
+                            "<span class='obvestilo label label-warning fade-in'>" +
+                            "Ni podatkov!</span>");
+                        }
+                    },
+                    error: function() {
+                        $("#saveFeedback").html(
+                        "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+                        JSON.parse(err.responseText).userMessage + "'!");
+                    }
+                });
+                
+                $.ajax({
+                    url: baseUrl + "/view/" + ehrId + "/" + "blood_pressure",
+                    type: 'GET',
+                    headers: {"Ehr-Session": sessionId},
+                    success: function (res) {
+                        if (res.length > 0) {
+                            dataMascobaPas = res;
+                            document.getElementById('obsegPasu').value = dataMascobaPas[0].systolic;
+                            document.getElementById('mascoba').value = dataMascobaPas[0].diastolic;
+                            // console.log(dataMascobaPas);
+                            normalMascoba = normalize(dataMascobaPas, "mascoba");
+                        } else {
+                            $("#saveFeedback").html(
+                            "<span class='obvestilo label label-warning fade-in'>" +
+                            "Ni podatkov!</span>");
+                        }
+                    },
+                    error: function() {
+                        $("#saveFeedback").html(
+                        "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+                        JSON.parse(err.responseText).userMessage + "'!");
+                    }
+                });
+                
+                $.ajax({
+                    url: baseUrl + "/view/" + ehrId + "/" + "height",
+                    type: 'GET',
+                    headers: {"Ehr-Session": sessionId},
+                    success: function (res) {
+                        if (res.length > 0) {
+                            dataHeight = res;
+                            document.getElementById('visina').value = dataHeight[0].height;
+                            document.getElementById('datum').value = dataHeight[0].time.substring(0,10);
+                            BAI();
+                            BMI();
+                            // console.log(dataHeight);
+                        } else {
+                            $("#saveFeedback").html(
+                            "<span class='obvestilo label label-warning fade-in'>" +
+                            "Ni podatkov!</span>");
+                        }
+                    },
+                    error: function() {
+                        $("#saveFeedback").html(
+                        "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+                        JSON.parse(err.responseText).userMessage + "'!");
+                    }
+                });
+
+	    	},
+	    	error: function(err) {
+	    		$("#saveFeedback").html(
+            "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+            JSON.parse(err.responseText).userMessage + "'!");
+	    	}
+		});
+	}
 }
 
+function normalize(data, type) {
+    console.log("Obdelujem podatke");
+    var mesci = ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Avg", "Sep", "Okt", "Nov", "Dec"];
+    
+    var output = [];
+    
+    for (var i = 0; i < data.length; i++) {
+        output[i] = {"label": null, "value": null};
+        var mm = parseInt(data[i].time.substring(5,7))-1;
+        output[i].label = mesci[mm];
+        if (type == "teza") {
+            output[i].value = data[i].weight;
+        }
+         else if (type == "mascoba") {
+             output[i].value = data[i].diastolic;
+         }
+    }
+    
+    console.log(output);
+    return output;
+   
+}
 
 function clearCreate() {
     document.getElementById('newIme').value = '';
@@ -302,194 +420,204 @@ function BAI() {
   }
 }
 
-FusionCharts.ready(function(){
-      var chartTeza = new FusionCharts({
-        "type": "column2d",
-        "renderAt": "chartContainerTeza",
+function grafTeza() {
+    FusionCharts.ready(function(){
+        var podatki = [
+                {
+                    "label": "Jan",
+                    "value": "102"
+                },
+                {
+                    "label": "Feb",
+                    "value": "101"
+                },
+                {
+                    "label": "Mar",
+                    "value": "98"
+                },
+                {
+                    "label": "Apr",
+                    "value": "98"
+                },
+                {
+                    "label": "May",
+                    "value": "101"
+                },
+                {
+                    "label": "Jun",
+                    "value": "98"
+                },
+                {
+                    "label": "Jul",
+                    "value": "95"
+                },
+                {
+                    "label": "Aug",
+                    "value": "95"
+                },
+                {
+                    "label": "Sep",
+                    "value": "92"
+                },
+                {
+                    "label": "Oct",
+                    "value": "90"
+                },
+                {
+                    "label": "Nov",
+                    "value": "91"
+                },
+                {
+                    "label": "Dec",
+                    "value": "87"
+                }
+            ];
+        var chartTeza = new FusionCharts({
+            "type": "column2d",
+            "renderAt": "chartContainerTeza",
+            "width": "500",
+            "height": "300",
+            "dataFormat": "json",
+            "dataSource": {
+            "chart": {
+                "caption": "Spremembe Teže po mescih",
+                "xAxisName": "Mesec",
+                "yAxisName": "Teza[kg]",
+                "theme": "fint"
+            },
+            "data": normalTeza
+            }
+        });
+
+        chartTeza.render();
+    })
+}
+// graf prikaza deleža maščobe
+function grafMascoba() {
+    FusionCharts.ready(function(){
+        var podatki = [{
+                "label": "Jan",
+                "value": "30.5"
+            }, {
+                "label": "Feb",
+                "value": "30.2"
+            }, {
+                "label": "Mar",
+                "value": "29.8"
+            }, {
+                "label": "Apr",
+                "value": "28.0"
+            }, {
+                "label": "Maj",
+                "value": "28.5"
+            }, {
+                "label": "Jun",
+                "value": "28.1"
+            }, {
+                "label": "Jul",
+                "value": "28.0"
+            }, {
+                "label": "Aug",
+                "value": "27.7"
+            }, {
+                "label": "Sep",
+                "value": "27.2"
+            }, {
+                "label": "Okt",
+                "value": "27.3"
+            }, {
+                "label": "Nov",
+                "value": "27.0"
+            }, {
+                "label": "Dec",
+                "value": "22.0"
+            }];
+        
+        var fusioncharts = new FusionCharts({
+        "type": "spline",
+        "dataFormat": "json",
+        "renderAt": "chartMascoba",
         "width": "500",
         "height": "300",
-        "dataFormat": "json",
         "dataSource": {
-          "chart": {
-              "caption": "Spremembe Teže po mescih",
-              "xAxisName": "Mesec",
-              "yAxisName": "Teza[kg]",
-              "theme": "fint"
-           },
-          "data": [
-              {
-                 "label": "Jan",
-                 "value": "102"
-              },
-              {
-                 "label": "Feb",
-                 "value": "101"
-              },
-              {
-                 "label": "Mar",
-                 "value": "98"
-              },
-              {
-                 "label": "Apr",
-                 "value": "98"
-              },
-              {
-                 "label": "May",
-                 "value": "101"
-              },
-              {
-                 "label": "Jun",
-                 "value": "98"
-              },
-              {
-                 "label": "Jul",
-                 "value": "95"
-              },
-              {
-                 "label": "Aug",
-                 "value": "95"
-              },
-              {
-                 "label": "Sep",
-                 "value": "92"
-              },
-              {
-                 "label": "Oct",
-                 "value": "90"
-              },
-              {
-                 "label": "Nov",
-                 "value": "91"
-              },
-              {
-                 "label": "Dec",
-                 "value": "87"
-              }
-           ]
+            "chart": {
+                "caption": "Odstotek Masčobe",
+                "xAxisName": "Mesec",
+                "yAxisName": "Delež Maščobe v %",
+
+                //Cosmetics
+                "lineThickness": "3",
+                "paletteColors": "#008ee4,#6baa01",
+                "baseFontColor": "#333333",
+                "baseFont": "Helvetica Neue,Arial",
+                "captionFontSize": "14",
+                "subcaptionFontSize": "14",
+                "subcaptionFontBold": "0",
+                "showBorder": "0",
+                "showValues": "0",
+                "bgColor": "#ffffff",
+                "showShadow": "0",
+                "canvasBgColor": "#ffffff",
+                "canvasBorderAlpha": "0",
+                "divlineAlpha": "100",
+                "divlineColor": "#999999",
+                "divlineThickness": "1",
+                "divLineIsDashed": "1",
+                "divLineDashLen": "1",
+                "divLineGapLen": "1",
+                "showXAxisLine": "1",
+                "xAxisLineThickness": "1",
+                "xAxisLineColor": "#999999",
+                "showAlternateHGridColor": "0"
+            },
+            "data": normalMascoba
         }
+    }
+    );
+        fusioncharts.render();
     });
-
-    chartTeza.render();
-})
-// graf prikaza mišične mase
-FusionCharts.ready(function(){
-    var fusioncharts = new FusionCharts({
-    "type": "spline",
-    "dataFormat": "json",
-    "renderAt": "chartMascoba",
-    "width": "500",
-    "height": "300",
-    "dataSource": {
-        "chart": {
-            "caption": "Odstotek Masčobe",
-            "xAxisName": "Mesec",
-            "yAxisName": "Delež Maščobe v %",
-
-            //Cosmetics
-            "lineThickness": "3",
-            "paletteColors": "#008ee4,#6baa01",
-            "baseFontColor": "#333333",
-            "baseFont": "Helvetica Neue,Arial",
-            "captionFontSize": "14",
-            "subcaptionFontSize": "14",
-            "subcaptionFontBold": "0",
-            "showBorder": "0",
-            "showValues": "0",
-            "bgColor": "#ffffff",
-            "showShadow": "0",
-            "canvasBgColor": "#ffffff",
-            "canvasBorderAlpha": "0",
-            "divlineAlpha": "100",
-            "divlineColor": "#999999",
-            "divlineThickness": "1",
-            "divLineIsDashed": "1",
-            "divLineDashLen": "1",
-            "divLineGapLen": "1",
-            "showXAxisLine": "1",
-            "xAxisLineThickness": "1",
-            "xAxisLineColor": "#999999",
-            "showAlternateHGridColor": "0"
-        },
-        "data": [{
-            "label": "Jan",
-            "value": "30.5"
-        }, {
-            "label": "Feb",
-            "value": "30.2"
-        }, {
-            "label": "Mar",
-            "value": "29.8"
-        }, {
-            "label": "Apr",
-            "value": "28.0"
-        }, {
-            "label": "Maj",
-            "value": "28.5"
-        }, {
-            "label": "Jun",
-            "value": "28.1"
-        }, {
-            "label": "Jul",
-            "value": "28.0"
-        }, {
-            "label": "Aug",
-            "value": "27.7"
-        }, {
-            "label": "Sep",
-            "value": "27.2"
-        }, {
-            "label": "Okt",
-            "value": "27.3"
-        }, {
-            "label": "Nov",
-            "value": "27.0"
-        }, {
-            "label": "Dec",
-            "value": "22.0"
-        }]
-    }
 }
-);
-    fusioncharts.render();
-});
-
 // graf rutine
- FusionCharts.ready(function(){
-    var fusioncharts = new FusionCharts({
-    type: 'doughnut2d',
-    renderAt: 'chartRutina',
-    width: '500',
-    height: '300',
-    dataFormat: 'json',
-    dataSource: {
-        "chart": {
-            "caption": "Tednska Rutina",
-            "subCaption": "Predlog razdelitve aktivnosti",
-            "startingAngle": "20",
-            "showPercentValues": "1",
-            "showPercentInTooltip": "0",
-            "enableSmartLabels": "1",
-            "enableMultiSlicing": "0",
-            "decimals": "0",
-            "useDataPlotColorForLabels": "1",
-            //Theme
-            "theme": "fint"
-        },
-        "data": [{
-            "label": "Trening z utežmi",
-            "value": "20"
-        }, {
-            "label": "Fizioterapija",
-            "value": "30"
-        }, {
-            "label": "Vaje za motoriko",
-            "value": "10"
-        }, {
-            "label": "Počitek",
-            "value": "40"
-        }]
+    function grafRutina() {
+    FusionCharts.ready(function(){
+        var podatki = [{
+                "label": "Trening z utežmi",
+                "value": "20"
+            }, {
+                "label": "Fizioterapija",
+                "value": "30"
+            }, {
+                "label": "Vaje za motoriko",
+                "value": "10"
+            }, {
+                "label": "Počitek",
+                "value": "40"
+            }];
+            
+        var fusioncharts = new FusionCharts({
+        type: 'doughnut2d',
+        renderAt: 'chartRutina',
+        width: '500',
+        height: '300',
+        dataFormat: 'json',
+        dataSource: {
+            "chart": {
+                "caption": "Tednska Rutina",
+                "subCaption": "Predlog razdelitve aktivnosti",
+                "startingAngle": "20",
+                "showPercentValues": "1",
+                "showPercentInTooltip": "0",
+                "enableSmartLabels": "1",
+                "enableMultiSlicing": "0",
+                "decimals": "0",
+                "useDataPlotColorForLabels": "1",
+                //Theme
+                "theme": "fint"
+            },
+            "data": podatki
+        }
     }
+    );
+        fusioncharts.render();
+    });
 }
-);
-    fusioncharts.render();
-});
